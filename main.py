36141a98b65a719e8ac203c883a25b5ea3b373b9
gholbrook <geremiah.holbrook@gmail.com>
2026-05-14 22:38:14 -0400

    first working draft

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ urls = [
     'https://www.wowvegas.com/',
     'https://www.chumbacasino.com/',
     'https://funrize.com/',
-    'https://acebet.cc/',
     'https://luckystake.com/'
 ]
 
@@ -27,51 +26,122 @@ claim_steps = {
             # ...
         ],
         'claim': [
-            # click (x, y)
-            # wait some time
-            # click (x, y)
-            # ...
+            (935, 723)
         ]
     },
     'https://realprize.com/': {
         'login': [],
-        'claim': []
+        'claim': [
+            (962, 909)
+        ]
     },
     'https://lonestarcasino.com/': {
         'login': [],
-        'claim': []
+        'claim': [
+            (956, 854)
+        ]
     },
     'https://taofortune.com/': {
         'login': [],
-        'claim': []
+        'claim': [
+            (45, 170),
+            (70, 286),
+            (828, 514),
+            (45, 170),
+            (94, 327),
+            (952, 825)
+        ]
     },
     'https://nolimitcoins.com/': {
         'login': [],
-        'claim': []
+        'claim': [
+            (1146, 452),
+            # click menu button
+            (45, 155),
+            (964, 596),
+            (959, 751),
+            (1147, 381),
+            # click menu button
+            (45, 155),
+            # click daily reward
+            (126, 581),
+            # click claim rewards
+            (959, 813),
+            # click collect
+            (952, 797)
+        ]
     },
     'https://fortunewins.com/': {
-        'login': [],
-        'claim': []
+        'login': [
+            # click login
+            (1370, 161),
+            # click next login
+            (1229, 680)
+        ],
+        'claim': [
+            # click get coins
+            (1370, 147),
+            # click free coins
+            (1091, 248),
+            # click collect
+            (536, 651)
+        ]
     },
     'https://www.wowvegas.com/': {
         'login': [],
-        'claim': []
+        'claim': [
+            # click menu
+            (42, 159)
+        ]
     },
     'https://www.chumbacasino.com/': {
-        'login': [],
-        'claim': []
+        'login': [
+            # click login
+            (1265, 195),
+            # click next login
+            (275, 672)
+        ],
+        'claim': [
+            # click get coins
+            (1695, 149),
+            # click daily bonus
+            (1057, 260),
+            # scroll down dunno yet
+            (-1, -1),
+            # click claim
+            (951, 862)
+        ]
     },
     'https://funrize.com/': {
         'login': [],
-        'claim': []
-    },
-    'https://acebet.cc/': {
-        'login': [],
-        'claim': []
+        'claim': [
+            # click free spin
+            (41, 352),
+            # click spin
+            (962, 504),
+            # click x
+            (1397, 195),
+            # click daily rewards
+            (92, 406),
+            # click collect
+            (729, 792)
+        ]
     },
     'https://luckystake.com/': {
-        'login': [],
-        'claim': []
+        'login': [
+            # click login
+            (1375, 182),
+            # click email box
+            (937, 606),
+            # click email
+            (951, 652),
+            # click sign in
+            (957, 766)
+        ],
+        'claim': [
+            # click claim
+            (964, 863)
+        ]
     }
 }
 
@@ -98,7 +168,7 @@ def log_in():
     pass
 
 for url in urls:
-    time.sleep(10)
+    time.sleep(2)
     navigate_to_url(url)
 
     if logged_in:
@@ -107,4 +177,9 @@ for url in urls:
         log_in()
 
     # claim daily rewards
-    time.sleep(10)
+    for step in claim_steps[url]['claim']:
+        print(step[0], step[1])
+        if step[0] == -1 and step[1] == -1:
+            pag.scroll(-3)
+        pag.click(step[0], step[1])
+        time.sleep(5)
